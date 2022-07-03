af23e9da1369d69f62108f15630d69eac24ab554
Yasmin Marques Vieira <91732850+yasminMVieira@users.noreply.github.com>
2022-07-03 20:24:52 -0300

    2.0

--- PortalAdm.java
+++ PortalAdm.java
@@ -5,12 +5,12 @@
  */
 package portal;
 
+import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import java.lang.String;
-import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -23,7 +23,6 @@ public class PortalAdm {
 	    Scanner s = new Scanner(System.in);
 
         try {
-
 
             //-----
             int servidor = 0;
@@ -52,7 +51,7 @@ public class PortalAdm {
                     case 1:
                         id = criaId();
                         System.out.printf("Id do cliente: "+id);
-                        System.out.println("\n-Digite os dados do cliente-");
+                        System.out.println("\n--Digite os dados do cliente--\n");
                         System.out.println("Idade: ");
                         int idade = s.nextInt();
                         System.out.println("CPF: ");
@@ -62,23 +61,20 @@ public class PortalAdm {
                         transport.open();
                         TProtocol protocol = new  TBinaryProtocol(transport);
                         Portal.Client client = new Portal.Client(protocol);
-                        
-                        //servidor = retornaServidorV(id);
-                        
+
                         retorno = client.inserirCliente(id, idade, cpf);
+
                         if(retorno == false){
                             System.out.println("\nOperação inválida, cliente já existe!\n");
                         } 
                         else{
                             System.out.println("\nCliente adicionado com sucesso!\n");    
                         }   
-                        System.out.println(retorno);
-                        transport.close() ;
-                    //-----
-                                                
+
+                        transport.close();
                     break;
                     case 2:
-                        System.out.println("-Digite os dados do cliente-");
+                        System.out.println("\n--Digite os dados do cliente--\n");
                         System.out.println("Id: ");
                         id = s.next();
                         System.out.println("Idade: ");
@@ -92,18 +88,19 @@ public class PortalAdm {
                         client = new Portal.Client(protocol);
 
                         retorno = client.modificarCliente(id, idade, cpf);
+
                         if(retorno == true){
                             System.out.println("\nCliente modificado com sucesso!\n");
                         } 
                         else{
                             System.out.println("\nCliente não existe\n ");
                         }  
-                        System.out.println(retorno);
-                        transport.close() ;
+
+                       transport.close();
                     break;
                         
                     case 3: 
-                        System.out.println("Informe o id do cliente a ser removido:");
+                        System.out.println("\nInforme o id do cliente a ser removido:");
                         id = s.next();
 
                         transport = new TSocket("localhost", 9090);
@@ -112,16 +109,16 @@ public class PortalAdm {
                         client = new Portal.Client(protocol);
 
                         retorno = client.apagarCliente(id);
+
                         if(retorno == true)
                             System.out.println("\nCliente removido com sucesso!\n");
                         else
                             System.out.println("\nCliente nao encontrado!\n");
-                        System.out.println(retorno);   
+
                         transport.close();
                     break;
-                    
                     case 4:
-                        System.out.println("Informe o id do cliente a ser recuperado:");
+                        System.out.println("\nInforme o id do cliente a ser recuperado:");
                         id = s.next();
 
                         transport = new TSocket("localhost", 9090);
@@ -142,13 +139,15 @@ public class PortalAdm {
                             System.out.println("Cliente não está na hash");
                             break;
                         }
+
+                        transport.close();
                     break;
                     case 5:
                         exit(1);
                     default:
                         System.out.println("Esta opção nao é valida\n");
                 }
-            }while(opcao != -1); 
+            }while(opcao != -1);
         }catch (IdNotFound i) {
             System.err.println("ID not found: " + i.getId());
         } catch (Exception x) {
@@ -161,5 +160,30 @@ public class PortalAdm {
     public static String criaId() {
         String uniqueID = UUID.randomUUID().toString();
         return uniqueID;
+    }
+
+    public static String receberCliente(int idade, int cpf) throws TException { //invocado quando o cliente é autenticado (PortalClient)
+
+        boolean retorno;
+        String id = criaId();
+
+        TTransport transport = new TSocket("localhost", 9090);
+        transport.open();
+        TProtocol protocol = new TBinaryProtocol(transport);
+        Portal.Client client = new Portal.Client(protocol);
+
+        retorno = client.inserirCliente(id, idade, cpf);
+
+        if(retorno == false){
+            System.out.println("\nCliente já existe!\n");
+            transport.close();
+            return null;
+        }
+        else{
+            System.out.println("\nCliente adicionado com sucesso!\n");
+            transport.close();
+            return id;
+        }
+
     }
 }
